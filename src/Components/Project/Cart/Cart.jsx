7d21b245c79387f2f9b10c78ea88b27dec954b0c
Lovely Jesus <desappglobal1@gmail.com>
2024-04-23 13:48:54 +0100

    first commit

---
+++ src/Components/Project/Cart/Cart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Cart.css";
+
+function Cart() {
+  return (
+    <div className="Cart">
+      <nav>
+        <ul>
+          <h1>Free Shipping from $99.00</h1>
+          <h2>money Guarantee 30 days back</h2>
+          <h3>Payment Method secure system</h3>
+          <h4>Online Support 24 hours daily</h4>
+          <h5>100% Safe secure shipping</h5>    
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Cart
